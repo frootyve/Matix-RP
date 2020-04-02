8b5dbf9eda654b79fa477984c2709bbdb07ad94e
frootyve <52638502+frootyve@users.noreply.github.com>
2020-04-02 21:58:07 +0200

    Matix

---
+++ index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MatixRP</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="CSS/style.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	</head>
+
+
+
+<body>
+
+  <!-- Début de la NavBar!-->
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #363535 !important;">
+  <a class="navbar-brand" href="index.php">MATIX RP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fas fa-bars"></i>
+  </button>
+  <button class="btn btn-info" style="margin-left: 30px;">Connexion</button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Acceuil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Actuallité</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Boutique</a>
+      </li>
+       <li class="nav-item"
+>        <a class="nav-link" href="staff.php">Staff</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">WorkShop</a>
+      </li>
+    </ul>
+     </div>
+	</nav>
+
+
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="IMG/banner1.png" class="d-block w-100" alt="...">
+       <div class="carousel-caption d-none d-md-block">
+        <h5 class="animated bounceInRight" style="animation-delay:1s;">Découvrer Notre Univers</h5>
+        <p class="animated bounceInLeft" style="animation-delay: 2s;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <a href="" class="animated bounceInRight" style="animation-delay:2s;">Nous rejoindre !</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="IMG/2.jpg" class="d-block w-100" alt="...">
+       <div class="carousel-caption d-none d-md-block">
+        <h5 class="animated slideInDown" style="animation-delay:1s;">Un serveur totalement différent</h5>
+        <p class="animated fadeInUp" style="animation-delay:2s;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <a href="" class="animated zoomIn" style="animation-delay:3s;">Nous rejoindre !</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="IMG/3.jpg" class="d-block w-100" alt="...">
+       <div class="carousel-caption d-none d-md-block">
+        <h5 class="animated zoomIn" style="animation-dleay:1s;">Met ce que tu veut ici mec</h5>
+        <p class="animated fadeInLeft" style="animation-delay:2s;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <a href="" class="animated zoomIn" style="animation-delay:3s;">Nous rejoindre !</a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!--Fin de la NavBar!-->
+
+
+
+<!--début des cards!-->
+<br>
+<br>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-12">
+					<div class="row row-grid">
+						<div class="col-lg-4">
+							<div class="card card-lift--hover shadow border-0">
+								<div class="card-body py-5">
+                 <div class="icon" style="background-color: #009bff; width: 90px; height: 90px; border-radius: 90px; margin-left: 110px;">
+                    <i class="fab fa-goodreads" style=" font-size: 45px;transform: translateY(50%); margin-left: 26px; margin-bottom: 5px; color: white;"></i>
+                </div>
+									<div class="icon icon-shape bg-gradient-green rounded-circle text-white mb-4">
+									</div>
+									<h6 class="text-primary text-uppercase">Les meilleurs addons</h6>
+									<p class="description mt-3">
+									Nous avons sélectionné les meilleurs addons pour vous rendre un roleplay plus immersif !<br><br>
+									</p>
+									</div>
+							</div>
+						</div>
+
+		<div class="col-lg-4">
+			<div class="card card-lift--hover shadow border-0">
+				<div class="card-body py-5">
+          <div class="icon" style="background-color: #009bff; width: 90px; height: 90px; border-radius: 90px; margin-left: 110px;">
+            <i class="fas fa-rocket" style=" font-size: 50px;transform: translateY(50%); margin-left: 15px; color: white;"></i>
+          </div>
+					<div class="icon icon-shape bg-gradient-primary rounded-circle text-white mb-4">					</div>
+					<h6 class="text-primary text-uppercase">Des performances extrêmes</h6>
+					<p class="description mt-3">
+					Nos serveurs sont hébergés sur les meilleures machine OVH qui sont sécurisés par des professionnels !
+					</p>
+					<br>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-4">
+			<div class="card card-lift--hover shadow border-0">
+				<div class="card-body py-5">
+           <div class="icon" style="background-color: #009bff; width: 90px; height: 90px; border-radius: 90px; margin-left: 110px;">
+            <i class="fas fa-user-plus" style=" font-size: 45px;transform: translateY(50%); margin-left: 15px; color: white;"></i>
+          </div>
+					<div class="icon icon-shape bg-gradient-primary rounded-circle text-white mb-4">
+					</div>
+					<h6 class="text-primary text-uppercase">Une équipe compétente</h6>
+					<p class="description mt-3">
+					Nos administrateurs travaillent à plein temps pour vous rendre une meilleure expérience roleplay !
+					</p>
+					<br>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+<!--Fin des cards!-->
+
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<!--Debut zone acutallité!-->
+<section class="section bg-secondary" style="background-color: #f4f5f7 !important; ">
+  <h2 style="text-align: center;">Actuallité du serveur</h2>
+  <br>
+  <br>
+	<div class="row no-gutters bg-light position-relative">
+  <div class="col-md-3 mb-md-2 p-md-2">
+    <img src="IMG/logo.png" class="w-100" alt="..." style=" margin-left: 20px; width: 100%; height: 100%;">
+  </div>
+    <div class="col-md-6">
+      <div class="pl-md-5">
+        <h3>Ouverture du Serveur !</h3>
+          <p class="lead">
+            Met ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veut
+          </p>
+          <p>
+           Met ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veut
+          </p>
+          <a href="" target="_blank" class="font-weight-bold text-succes mt-5" rel="noopener">
+          En savoir plus !
+        </a>
+      </div>
+    </div>
+  </div>
+  </section>
+  <!--Fin de la zone d'acutallité!-->
+
+
+<br>
+<br>
+<br>
+
+
+<!--Petite annonce!-->
+  <section class="section section-lg">
+      <div class="container">
+        <div class="row row-grid justify-content-center">
+          <div class="col-lg-8 text-center">
+          <h2 class="display-3">
+          <span class="text-primary" style="font-size: 5vh;">N'hésitez pas à visisité notre boutique !</span>
+          </h2>
+          <br>
+          <p class="lead">
+        Met ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veut
+          </p>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Fin de la petie annonce!-->
+
+<br>
+<br>
+<br>
+<hr>
+<br>
+
+
+  <!--Container!-->
+  <section class="section section-lg pt-0">
+    <div class="container">
+      <div class="card bg-gradient-warning shadow-lg border-0" style="background-color: #1488CC!important;">
+        <div class="p-5">
+          <div class="row align-items-center">
+            <div class="col-lg-8">
+              <h3 class="text-white">Connectez vous sur Steam pour plus de nouveauté !</h3>
+              <p class="lead text-white mt-3">
+              Simple et efficace, connectez vous dès aujourd'hui pour recevoir toutes les nouveautés et information à propos du serveur.
+              </p>
+            </div>
+          <div class="col-lg-3 ml-lg-auto">
+          <a href="#" class="btn btn-lg btn-block btn-white" style="background-color: #FFF; ">SE CONNECTER AVEC STEAM</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+ <!--Fin Container!-->
+<br>
+<hr>
+<br>
+<br>
+<br>
+
+<!--Partie Discord!-->
+      <section class="section section-lg">
+      <div class="container">
+        <div class="row row-grid justify-content-center">
+          <div class="col-lg-8 text-center">
+          <h2 class="display-3">
+          <span class="animated bounceInUp text-success" style="font-size: 40px; animation-delay: 1s;">Rejoignez Notre Discord !</span>
+          </h2>
+          <br>
+          <p class="lead">
+         Met ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veutMet ce que tu veut
+          </p>
+          <a href="https://discord.gg/64V59k7" class="btn btn-lg btn-block btn-white" style="background-color: #FFF; ">Cliquez-ici pour rejoindre le discord !</a>
+          <div>
+            <br>
+            <iframe src="https://discordapp.com/widget?id=521785836124110850&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Fin Partie Discord!-->
+<br>
+<br>
+<br>
+
+
+
+<!--Footer!-->
+<div class="bottom section-padding">
+      <div class="container">
+        <div class="row row-grid">
+          <div class="col-md-12 left">
+            <div class="copyright">
+              <p>© <span>2020</span> <a href="#" class="transition">MATIX</a> All rights reserved./ développé par <a href="#">Frootyve#4456</a></p>
+              <div class="col-md-12 right">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Fin footer!-->
+
+
+
+
+  <!--Script !-->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ staff.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MatixRP</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="CSS/style.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	</head>
+
+
+
+
+<body>
+  <!-- Début de la NavBar!-->
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #363535 !important;">
+  <a class="navbar-brand" href="index.php">MATIX RP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <button class="btn btn-info" style="margin-left: 30px;">Connexion</button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Acceuil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Actuallité</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Boutique</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Staff</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">WorkShop</a>
+      </li>
+    </ul>
+     </div>
+	</nav>
+<br>
+<br>
+<br>
+<br>
+<section>
+  <div class="container">
+    <h1 style="text-align: center;">Le staff</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <hr>
+    <h2 style="text-align: center; color: red;">FONDATEUR</h2>
+  <div class="card" style="width: 18rem; margin-left: 43vh;">
+  <img src="IMG/logo.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 style="color: red; text-align: center;">LYZZER</h3>
+    <br>
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+    <p class="card-text"><small class="text-muted">Fondateur/Chef du projet</small></p>
+  </div>
+</div>
+<br>
+<hr>
+<h2 style="color: #800000; text-align: center;">MANAGER</h2>
+<div class="row row-cols-1 row-cols-md-3" style="margin-left: 28vh;">
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h4 class="card-title" style="color: #800000; text-align: center; ">XERON</h4>
+        <br>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+        <p class="card-text"><small class="text-muted">Manager</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/source2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h4 class="card-title" style="color: #800000; text-align: center;">GEDEY</h4>
+        <br>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+        <p class="card-text"><small class="text-muted">Manager</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<hr>
+<br>
+<h2 style="text-align: center;">ADMINISTRATEUR</h2>
+<div class="card-deck">
+  <div class="card">
+    <img src="IMG/logo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">MAKO</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+      <p class="card-text"><small class="text-muted">Responsable Administrateur</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="IMG/logo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">EOLE</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+      <p class="card-text"><small class="text-muted">Administrateur</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="IMG/logo.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">RICO TESTINE</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+      <p class="card-text"><small class="text-muted">Administrateur</small></p>
+    </div>
+  </div>
+</div>
+<br>
+<hr>
+<br>
+<h2 style="text-align: center;">MODERATEUR/HELPEUR</h2>
+<div class="row row-cols-1 row-cols-md-4">
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">MAX McKENZIE</h5>
+        <p class="card-text">J’ai rejoint le staff de MatixRP afin d’aider les joueurs qui en ont besoin et ainsi leur proposer la meilleure expérience de jeu possible.</p>
+        <p class="card-text"><small class="text-muted">Responsable-Modérateur</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/source3.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ILLUSION</h5>
+        <p class="card-text">j'ai rejoint le staff de MatixRP afin d'en faire un serveur respectable et respecté pour et par la communauté. Je ferai tous mon possible pour que le serveur soit confortable</p>
+        <p class="card-text"><small class="text-muted">Helpeur</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">VLAD KARKAKOV</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet.</p>
+        <p class="card-text"><small class="text-muted">Helpeur</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/source4.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">NERIMALF</h5>
+        <p class="card-text">J'ai rejoins le projet MatixRP car cela fait longtemps que je cherche un serveur sur le quel me poser qui à des bons addons et une map unique.
+                    J'en avais marre des serveurs qui avait tous les mêmes addons et la même map.
+                    Et je chercher aussi un serveur sur lequel le staff n'est pas "toxic".
+                    Voilà pourquoi j'ai rejoins le projet MatixRP.</p>
+        <p class="card-text"><small class="text-muted">Helpeur</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<hr>
+<br>
+<h2 style="text-align: center;">DEVELOPPEUR</h2>
+<div class="row row-cols-1 row-cols-md-4">
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/source.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">FROOTYVE</h5>
+        <p class="card-text">J'ai rejoint le projet de "MATIXRP" car celui-ci me tient à coeur. Le poste de développeur web m'offre l'opportunité de mettre en avant mes capacités en développement mais aussi d'aboter un soutien à la communauté.
+         </p>
+        <p class="card-text"><small class="text-muted">Développeur Web</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">BLU</h5>
+        <p class="card-text">J'ai rejoint le staff de MatixRP afin de développer le   serveur et y avoir un créer publique pour qu'il puisse y avoir de la visibilité, en rejoignant le staff je contribue au bon développement du serveur ainsi qu'à son amélioration constante.</p>
+        <p class="card-text"><small class="text-muted">Développeur Web</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">FLEODON</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet.</p>
+        <p class="card-text"><small class="text-muted">Développeur G-LUA</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">HELLASTOK</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+        <p class="card-text"><small class="text-muted">Développeur BOT</small></p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <img src="IMG/logo.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">LeMEC</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet</p>
+        <p class="card-text"><small class="text-muted">Développeur G-LUA</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+<br>
+<hr>
+<br>
+
+
+<section class="section section-lg pt-0">
+    <div class="container">
+      <div class="card bg-gradient-warning shadow-lg border-0" style="background-color: #009933!important;">
+        <div class="p-4">
+          <div class="row align-items-center">
+            <div class="col-lg-8">
+              <h3 class="text-white">Envie de Rejoindre le Staff ?</h3>
+              <p class="lead text-white mt-3">
+              BlablabblablaBlablabblablaBlablabblablaBlablabblablaBlablabblablaBlablabblablaBlablabblablaBlablabblablaBlablabblabla
+              </p>
+            </div>
+          <div class="col-lg-3 ml-lg-auto">
+          <a href="#" class="btn btn-lg btn-block btn-white" style="background-color: #FFF; ">REJOINDRE LE STAFF !</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<br>
+<hr>
+<br>
+
+
+
+
+
+
+
+<div class="bottom section-padding">
+      <div class="container">
+        <div class="row row-grid">
+          <div class="col-md-12 left">
+            <div class="copyright">
+              <p>© <span>2020</span> <a href="#" class="transition">MATIX</a> All rights reserved./ développé par <a href="#">Frootyve#4456</a></p>
+              <div class="col-md-12 right">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
